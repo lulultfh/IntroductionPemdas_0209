0e9eeeb26399a786580ef7a025d7c0598b0ffe07
lulultfh <punyalulu05@gmail.com>
2026-02-23 15:10:05 +0700

    menambahkan variable age1 yang bertipekan integer

--- coba.cpp
+++ coba.cpp
@@ -7,9 +7,12 @@ string nama;
 
 int main(){ //program utama
     int age; //variable local
+    int age1; //variable local
     age = 20;
+    age1 = 15;
     
     cout << "Halo World" << endl;
     cout << 99 << endl;
     cout << age;
+    cout << age1;
 }
