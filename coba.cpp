97a151bc4c1f278e9988af4f49bba3ac46cde037
lulultfh <punyalulu05@gmail.com>
2026-02-23 15:06:08 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,15 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string nama;
+
+int main(){ //program utama
+    int age; //variable local
+    age = 20;
+    
+    cout << "Halo World" << endl;
+    cout << 99 << endl;
+    cout << age;
+}
